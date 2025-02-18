6b392c9c9c493d0438db208bcedcb837886d7a53
ahuja012002 <60950537+ahuja012002@users.noreply.github.com>
2025-02-17 22:21:28 -0500

    iniital commit Deepseek R1 on groq with AWS ELB

---
+++ src/main/java/com/example/Springboot_deepseek_api/ChatController.java
@@ -0,0 +1,29 @@
+package com.example.Springboot_deepseek_api;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+@CrossOrigin
+public class ChatController {
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+   
+
+    @GetMapping("/stream")
+    public Flux<String> chatWithStream(@RequestParam String message) {
+        return chatClient.prompt()
+                .user(message)
+                .stream()
+                .content();
+    }
+}
---
+++ src/main/java/com/example/Springboot_deepseek_api/SpringbootDeepseekApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_deepseek_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDeepseekApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDeepseekApiApplication.class, args);
+	}
+
+}
